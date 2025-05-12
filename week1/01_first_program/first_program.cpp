807c91e96b82c4bdeb6e97262682cf28a9de8f24
ReedST7 <rtaylor7685@floridapoly.edu>
2025-05-12 17:51:19 +0000

    Create first program

---
+++ week1/01_first_program/first_program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main() {
+
+    // Print a welcome message
+    std::cout << "Hello COP3337!\n" << std::endl;
+    
+    // cout - console output
+    // endl - new line
+    // << stream insertion operator
+    // :: scope resolution operator
+    // std - standard namespace
+
+    std::cout << "5 / 2 is " << 5.0 / 2 << std::endl;
+
+    return 0;
+}
